5017f7d00808971d88fca9a3444bc371d0efd184
sieger.verdonck <sieger.verdonck@student.howest.be>
2022-04-27 11:42:12 +0200

    created Webshop Views, ViewModels and Controllers

---
+++ Models/ListingsViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace mtg_app.Models {
+    public class ListingsViewModel {
+        public string PageTitle {get; set;}
+        public string LinkTitle {get; set;}
+
+        public List<ListingViewModel> Listings {get; set;}
+    }
+}
