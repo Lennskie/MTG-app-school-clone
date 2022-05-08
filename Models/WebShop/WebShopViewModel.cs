737b4d8b0486b3936b291f9424534380bf711470
Milan Lagae <milan.lagae@student.howest.be>
2022-05-08 16:35:43 +0200

    Changed names of some views & completed func to add packs to cart

---
+++ Models/WebShop/WebShopViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace mtg_app.Models.WebShop;
+
+public class WebShopViewModel
+{
+    
+    public int? AmountOfPacksInCart { get; set; }
+    
+    public int CoinBalance { get; set; }
+    
+    public int BoosterPackPrice { get; set; }
+    
+    public int AmountOfPacksToAdd { get; set; }
+    
+}
---
+++ Services/SessionService.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace mtg_app.Services;
+
+public class SessionService
+{
+
+    public void StoreCartPacks(ISession session, int amountOfPacks)
+    {
+        int currentAmountOfPacks = GetAmountOfPacksCart(session);
+        
+        storeSessionData(session, "Cart", currentAmountOfPacks + amountOfPacks);
+    }
+
+
+    public int GetAmountOfPacksCart(ISession session)
+    {
+        string? amountOfPacksString = session.GetString("Cart");
+
+        if (amountOfPacksString == null)
+        {
+            return 0;
+        }
+        
+        int amountOfPacks = int.Parse(amountOfPacksString);
+
+        return amountOfPacks;
+    }
+    
+    
+    public void ClearSession(ISession session)
+    {
+        session.Clear();
+    }
+
+    private void storeSessionData(ISession session, string key, object value)
+    {
+        session.SetString(key, JsonSerializer.Serialize(value));
+    }
+    
+    
+}
