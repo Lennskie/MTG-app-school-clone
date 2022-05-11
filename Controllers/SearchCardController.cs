e57ee5d1273e69a50ae5fedd459a2e890af1d206
lennc <lenn.crochart@student.howest.be>
2022-05-11 10:41:36 +0200

    Added extra Controller, Model & View for searching a card by name due to conflicts with id-search. TODO: fix SearchCardController error on line 25.

---
+++ Controllers/SearchCardController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using mtg_app.Models.Card;
+using mtg_lib.Library.Models;
+using mtg_lib.Library.Services;
+
+namespace mtg_app.Controllers
+{
+
+    [Route("[controller]")]
+    public class CardSearchController : Controller
+    {
+        CardService cardService = new CardService();
+
+        // Route: /SearchCard/<cardName>
+        [Authorize]
+        [Route("{cardName}")]
+        public IActionResult SingleCard(string cardName)
+        {
+            Card? card = cardService.GetCardFromString(cardName);
+
+            if (card?.OriginalImageUrl == null)
+            {
+                //Console.WriteLine("Retrieving new imageUrl");
+                card.OriginalImageUrl =  cardService.GetImageFromVariations(card);
+            }
+            
+            return View(new SingleCard
+            {
+                ImageUrl = card?.OriginalImageUrl,
+                Name = card?.Name,
+                Type = card?.Type,
+                Variations = cardService.RetrieveMtgIdsFromString(card)
+            });
+        }
+    }
+}
